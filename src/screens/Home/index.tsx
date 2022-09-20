8a808ea8e1f59dc77e07943a9fd5f47a6ff82a37
Joel Luis <joelluis73@gmail.com>
2022-09-20 15:36:43 -0300

    integração esports-server

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,12 +1,23 @@
-import { View, Image, FlatList } from 'react-native'
+import { useEffect, useState } from 'react'
+
 import logoImg from '../../assets/logo-nlw-esports.png'
+
+import { View, Image, FlatList } from 'react-native'
 import { GameCard } from '../../components/GameCard'
 import { Heading } from '../../components/Heading'
-import { GAMES } from '../../utils/games'
 
+import { GameCardProps } from '../../@types/listGames'
 import { styles } from './styles'
 
 export function Home() {
+  const [games, setGames] = useState<GameCardProps[]>([])
+
+  useEffect(() => {
+    fetch('http://172.18.0.1:3333/games')
+      .then((response) => response.json())
+      .then((data) => setGames(data))
+  }, [])
+
   return (
     <View style={styles.container}>
       <Image source={logoImg} style={styles.logo} />
@@ -15,7 +26,7 @@ export function Home() {
         subtitle="Selecione o game que deseja jogar..."
       />
       <FlatList
-        data={GAMES}
+        data={games}
         keyExtractor={(item) => item.id}
         renderItem={({ item }) => <GameCard data={item} />}
         showsHorizontalScrollIndicator={false}
