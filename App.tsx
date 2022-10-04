d3d2e22ddfa49fed3031263d71a275852c581237
Joel Luis <joelluis73@gmail.com>
2022-10-04 11:26:02 -0300

    create push notification

--- App.tsx
+++ App.tsx
@@ -1,7 +1,13 @@
+import { useRef, useEffect } from 'react'
 import { StatusBar } from 'react-native'
 import { Background } from './src/components/Background'
 import { Loading } from './src/components/Loading'
 import { Routes } from './src/routes'
+
+import * as Notifications from 'expo-notifications'
+import './src/services/notificationsConfig'
+import { Subscription } from 'expo-modules-core'
+import { getPushNotificationToken } from './src/services/getPushNotificationToken'
 
 import {
   useFonts,
@@ -18,6 +24,37 @@ export default function App() {
     Inter_700Bold,
     Inter_900Black
   })
+
+  const getNotificationListener = useRef<Subscription>()
+  const responseNotificationListener = useRef<Subscription>()
+
+  useEffect(() => {
+    getPushNotificationToken()
+  })
+
+  useEffect(() => {
+    getNotificationListener.current =
+      Notifications.addNotificationReceivedListener((notification) => {
+        console.log(notification)
+      })
+    responseNotificationListener.current =
+      Notifications.addNotificationResponseReceivedListener((response) => {
+        console.log(response)
+      })
+    return () => {
+      if (
+        getNotificationListener.current &&
+        responseNotificationListener.current
+      ) {
+        Notifications.removeNotificationSubscription(
+          getNotificationListener.current
+        )
+        Notifications.removeNotificationSubscription(
+          responseNotificationListener.current
+        )
+      }
+    }
+  }, [])
 
   return (
     <Background>
